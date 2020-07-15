d432dff6316357d062fc7ce0f619425ab13a552c
Enmah4k <enmah4k@gmail.com>
2020-07-15 04:04:29 -0400

    WebAPI: DbContext creado

---
+++ WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace shoopsupermarket.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options){
+
+        }
+
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Estudiante> Estudiantes { get; set; }
+        public DbSet<Profesor> Profesores { get; set; }
+    }
+}
