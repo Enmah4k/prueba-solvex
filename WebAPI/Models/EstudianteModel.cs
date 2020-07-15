f347df84d870bf56fa145e952392e56a7681124b
Enmah4k <enmah4k@gmail.com>
2020-07-15 02:35:42 -0400

    WebAPI: modelo Estudiante terminado

--- WebAPI/Models/EstudianteModel.cs
+++ WebAPI/Models/EstudianteModel.cs
@@ -6,18 +6,19 @@ namespace WebAPI.Models
     public class Estudiante
     {
         [Key]
-        public int ES_ID {get; set;}
+        public int Id {get; set;}
 
         [Required]
         public string Nombre {get; set;}
+
         public string Nombre2 {get; set;}
 
         [Required]
         public string Apellido {get; set;}
+
         public string Apellido2 {get; set;}
 
-        public byte[] Foto {get; set;}
-
-        public string Grado {get; set;}
+        [Required]
+        public string Telefono {get; set;}
     }
 }
